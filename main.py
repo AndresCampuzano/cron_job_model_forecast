386da65e0860a9825672c578f560676907477071
Andres Campuzano <campuxano88@gmail.com>
2025-04-19 11:05:42 -0500

    extend forecasting period to 48 hours for temperature and humidity

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ model_temp.fit(df_temp)
 model_hum = Prophet(daily_seasonality=True, yearly_seasonality=False, weekly_seasonality=False)
 model_hum.fit(df_hum)
 
-# 5. Forecast next 24 hours (hourly intervals)
-future_temp = model_temp.make_future_dataframe(periods=24, freq='h')
-future_hum = model_hum.make_future_dataframe(periods=24, freq='h')
+# 5. Forecast next 48 hours (hourly intervals)
+future_temp = model_temp.make_future_dataframe(periods=48, freq='h')
+future_hum = model_hum.make_future_dataframe(periods=48, freq='h')
 
 forecast_temp = model_temp.predict(future_temp)
 forecast_hum = model_hum.predict(future_hum)
@@ -80,12 +80,12 @@ else:
 
 # Optional: show plots
 # model_temp.plot(forecast_temp)
-# plt.title("Temperature Forecast (24h)")
+# plt.title("Temperature Forecast (48h)")
 # plt.grid(True)
 # plt.show()
 #
 # model_hum.plot(forecast_hum)
-# plt.title("Humidity Forecast (24h)")
+# plt.title("Humidity Forecast (48h)")
 # plt.grid(True)
 # plt.show()
 
